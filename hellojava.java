01f48ecd8669aa8cf8dfbc1de0aa74b05a38d65f
pranavkirad <pranavkirad.model@gmail.com>
2026-06-15 19:43:21 +0530

    Hello File updated second time

--- hellojava.java
+++ hellojava.java
@@ -2,5 +2,6 @@ class hellojava{
     public static void main(String[] args) {
         System.out.println("Hello from Java");
         System.out.println("Code Updated");
+        System.out.println("Code Updated 2 time");
     }
 }
