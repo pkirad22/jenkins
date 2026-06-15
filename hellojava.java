d4fa55fe9086a733af762ab1da7465093055ac2f
pranavkirad <pranavkirad.model@gmail.com>
2026-06-15 20:27:01 +0530

    Addition update

--- hellojava.java
+++ hellojava.java
@@ -3,5 +3,9 @@ class hellojava{
         System.out.println("Hello from Java");
         System.out.println("Code Updated");
         System.out.println("Code Updated 2 time");
+
+        int a,b = 10;
+        int c = a + b;
+        System.out.println("Addition is "+ c);
     }
 }
